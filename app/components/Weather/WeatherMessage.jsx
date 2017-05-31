eadda817bbf4336e065ffcc94e99d4a81f83e58d
Quentin FONCK <quentin.fonck@internet.lu>
2017-05-31 13:51:18 +0200

    Switching files to stateless functions

--- app/components/Weather/WeatherMessage.jsx
+++ app/components/Weather/WeatherMessage.jsx
@@ -1,14 +1,11 @@
 var React = require('react');
 
 //view - presentational component
-var WeatherMessage = React.createClass({
-  render: function() {
-    var location = this.props.location;
-    var temp = this.props.temp;
-    return (
-      <h3>Il fait {temp}°C à {location}.</h3>
-    );
-  }
-});
+
+var WeatherMessage = ({temp, location}) => {
+  return (
+    <h3>Il fait {temp}°C à {location}.</h3>
+  );
+};
 
 module.exports = WeatherMessage;
